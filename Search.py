55d9e3e5603bec368425850a21459d0ab429f229
Zainab Aqdas <zainab.aqdasrizvi@gmail.com>
2017-12-07 13:32:19 -0500

    More csvs and the Search program.

--- Search.py
+++ Search.py
@@ -1,19 +1,21 @@
-import spacy
-import nltk.data
-from textblob import TextBlob
-import numpy as np
 import csv
-import itertools
-zip = getattr(itertools, 'izip', zip)
+import sys
+from textblob import TextBlob
+import spacy
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk.data
 
 file = open('CultureRelatedDiaognosticIssues.txt','r')
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
+searchWord = sys.argv[1]
 a = []
-names = []
-sentiments = []
-meanArr = []
-sentenceArr = []
+names=[]
+nameSet = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+freq = [7,4,2,2,7,4,4,3,4,5,1,5,8,1,3,14,2,10,1]
+sentiments=[]
+meanArr=[]
 
 for line in file:
     miniList = line.split("|")
@@ -26,7 +28,6 @@ for i in range(len(a)):
     chap = a[i]
     chapSents = []
     sentences = tokenizer.tokenize(chap)
-    sentenceArr.append(sentences)
     for sentence in sentences:
         line = TextBlob(sentence)
         chapSents.append(line.sentiment.subjectivity)
@@ -37,15 +38,23 @@ for j in range(len(sentiments)):
     mean = np.mean(sentArray)
     meanArr.append(mean)
 
-# with open('subjectivityScores.csv', 'w') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(zip(names, meanArr))
-# f.close()
-#
-with open('subjectivityScorebySentence.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(sentenceArr, sentiments))
-f.close()
+chapters = []
+for i in range(len(a)):
+    diagnosis = a[i]
+    match = diagnosis.find(searchWord)
+    if (match > -1):
+        chapters.append(i)
 
-print(len(sentiments))
-print()
+print(chapters)
+
+newChaps = []
+for k in range(len(names)):
+    newChaps.append('green')
+
+for i in range(len(names)):
+    for j in range(len(chapters)):
+        if (i == chapters[j]):
+            newChaps[i] = 'blue'
+
+plt.bar(range(len(names)), meanArr, color=newChaps)
+plt.show()
